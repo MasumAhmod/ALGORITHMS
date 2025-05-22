86e9517ef0b494739a8c8217fc2deac17bbc71d7
MasumAhmod <masumahmod332@gmail.com>
2025-05-22 21:36:52 +0600

    Update maximum sum subarray.cpp

--- kadane's algo/maximum sum subarray.cpp
+++ kadane's algo/maximum sum subarray.cpp
@@ -5,9 +5,6 @@ using namespace std;
 #define optimize() ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 #define ll long long
 #define endl '\n'
-#define pb push_back
-#define vi vector<int>
-#define vii vector<long long>
 #define yes cout << "YES" << endl
 #define no cout << "NO" << endl
 #define gcd __gcd
@@ -15,14 +12,14 @@ using namespace std;
 
 void solve()
 {
-    ll n;
+    long long n;
     cin >> n;
 
-    vii v(n);
+    vector<long long> v(n);
     for(auto &u : v) cin >> u;
 
-    ll ans = v[0];
-    ll sum = 0;
+    long long ans = v[0];
+    long long sum = 0;
     for(int i = 0; i < n; ++i)
     {
         sum += v[i];
@@ -38,7 +35,6 @@ void solve()
 
 int32_t main()
 {
-    ///cODE BY Masum
     optimize();
 
     //solve();
