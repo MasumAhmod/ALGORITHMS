982fa17381ca26512469116640c7644cf5b9edb3
MasumAhmod <masumahmod332@gmail.com>
2026-01-30 11:58:23 +0600

    Add files via upload

---
+++ Binary Search/variable.py
@@ -0,0 +1,15 @@
+Python 3.14.2 (tags/v3.14.2:df79316, Dec  5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> name = "masum"
+>>> print(name)
+masum
+>>> age = 22
+>>> print(age)
+22
+>>> cgpa = 3.8
+>>> print(cgpa)
+3.8
+>>> print("My name is " + name + "\nAnd my age is " , age , "\nAlso my cgpa is " , cgpa)
+My name is masum
+And my age is  22 
+Also my cgpa is  3.8
